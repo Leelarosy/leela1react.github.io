a3d518a5eaa2e4b504fced159944bc4c34a62ee1
Leelarosy <rosyleela@gmail.com>
2020-02-19 18:07:25 +0530

    first commit

---
+++ precache-manifest.4fc77467f4942b2eeb81150070c92ce2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a4254ce26fcdaf7f79f524e47fa778",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7764960b928121d39d8",
+    "url": "/static/css/2.01598ba5.chunk.css"
+  },
+  {
+    "revision": "f11735d1e85aca7448fa",
+    "url": "/static/css/main.adf3615a.chunk.css"
+  },
+  {
+    "revision": "c7764960b928121d39d8",
+    "url": "/static/js/2.b9b22c6b.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/static/js/2.b9b22c6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f11735d1e85aca7448fa",
+    "url": "/static/js/main.a1f19550.chunk.js"
+  },
+  {
+    "revision": "cd51db41fab79dbd4d67",
+    "url": "/static/js/runtime-main.1a3b1f4c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
